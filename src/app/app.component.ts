54deb0df2708b89483c27620547b251f585603fd
GERARD Alexandre <agerard57@protonmail.com>
2022-06-02 01:10:03 +0200

    fix: White screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, AfterViewChecked, ChangeDetectorRef } from "@angular/core";
+import { AuthService } from './services/auth.service';
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-	title = "app";
-}
+
+export class AppComponent implements AfterViewChecked {
+
+	constructor(public auth: AuthService,
+				private changeDetector: ChangeDetectorRef) { }
+  
+	ngAfterViewChecked(): void {
+	  this.changeDetector.detectChanges();
+	}
+  
+  }
+  
