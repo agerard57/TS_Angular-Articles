deefa108610f659d1dda22757893e17c0ef9f81f
GERARD Alexandre <agerard57@protonmail.com>
2022-06-02 22:36:28 +0200

    feat: Article-list

--- src/app/areas/article-list/article-list.component.ts
+++ src/app/areas/article-list/article-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from "app/shared/models/article.model";
+import { Comment } from "app/shared/models/comment.model";
+import { ToastComponent } from 'app/shared/toast/toast.component';
+import { ArticleService } from "../../services/article.service";
+import { CommentService } from "../../services/comment.service";
 
 @Component({
   selector: 'app-article-list',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-list.component.scss']
 })
 export class ArticleListComponent implements OnInit {
+  article = new Article();
+  articles: Article[] = []
+  comment = new Comment();
+  comments: Comment[] = []
 
-  constructor() { }
+  constructor(
+    private articleService: ArticleService,
+    private commentService: CommentService,
+		public toast: ToastComponent) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {		
+    this.getArticles();
+/*     this.getCommentsbyArticle(); */
   }
-
+	getArticles(): void {
+		this.articleService.getArticles().subscribe({
+			next: (articles) => (this.articles = articles),
+			error: (_error) =>
+				this.toast.setMessage("Error fetching articles", "danger"),
+		});
+	}
 }
