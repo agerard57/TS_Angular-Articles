4d5f39b45053944d5011a74b62679e24bbb1fd0d
GERARD Alexandre <agerard57@protonmail.com>
2022-06-02 22:39:53 +0200

    feat: Create pipe

---
+++ src/app/pipes/truncate/truncate.pipe.spec.ts
@@ -0,0 +1,17 @@
+import { TruncatePipe } from './truncate.pipe';
+
+describe('TruncatePipe', () => {
+  let pipe: TruncatePipe;
+
+  beforeEach(() => {
+    pipe = new TruncatePipe();
+  });
+
+  it('Should truncate', () => {
+    expect(pipe.transform('Hello World', 5)).toEqual('Hello...');
+  });
+
+  it('Should preserve the whole input', () => {
+    expect(pipe.transform('Hello', 10)).toEqual('Hello');
+  });
+});
