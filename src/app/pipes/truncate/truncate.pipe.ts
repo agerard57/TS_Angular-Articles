bcd4b1ae066765a730c3b492f8c2fe48c0c0c218
agerard57 <agerard57@protonmail.com>
2022-06-03 17:07:17 +0200

    feat: Add length pipe

--- src/app/pipes/truncate/truncate.pipe.ts
+++ src/app/pipes/truncate/truncate.pipe.ts
@@ -3,7 +3,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({
 	name: "truncate",
 })
-
 export class TruncatePipe implements PipeTransform {
 	transform(value: string, limit: number, ellipsis = "…") {
 		return value.length > limit ? value.substr(0, limit) + ellipsis : value;
