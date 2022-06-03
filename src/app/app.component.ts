29f6c2a20c7815a7205e6aa6620b52bc9f378be4
agerard57 <agerard57@protonmail.com>
2022-06-03 17:06:10 +0200

    feat: App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
-import { Component, AfterViewChecked, ChangeDetectorRef } from "@angular/core";
-import { AuthService } from './services/auth.service';
+import {
+	Component,
+	AfterViewChecked,
+	ChangeDetectorRef,
+	OnInit,
+} from "@angular/core";
+import { AuthService } from "./services/auth.service";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
-	styleUrls: ["./app.component.scss"]
+	styleUrls: ["./app.component.scss"],
 })
+export class AppComponent implements AfterViewChecked, OnInit {
+	constructor(
+		public auth: AuthService,
+		private changeDetector: ChangeDetectorRef,
+	) {}
 
-export class AppComponent implements AfterViewChecked {
-
-	constructor(public auth: AuthService,
-				private changeDetector: ChangeDetectorRef) { }
-  
-	ngAfterViewChecked(): void {
-	  this.changeDetector.detectChanges();
+	ngOnInit(): void {
+		this.auth.init();
 	}
-  
-  }
-  
+
+	ngAfterViewChecked(): void {
+		this.changeDetector.detectChanges();
+	}
+}
